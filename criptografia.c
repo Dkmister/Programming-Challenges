c4c23fc8b46dd973eb170ae46c51979c041d55d3
Vilmar Neto <vilmar.neto@gmail.com>
2016-08-29 16:59:17 -0300

    Melhor documentacao da funcao

--- criptografia.c
+++ criptografia.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
 #include<string.h>
 
+/*
+criptografa_abc => retorna uma string
+
+A função basicamente tem a ideia de usar uma chave 'abcdefghijklmnopqrstuvxyz'
+Que a principio a primeira letra vira a ultima
+a => z
+b => y
+Percorrendo o laço, pela metade e percorrendo o numero n, sendo o valor do tamanho da chave
+o numero m, o valor do tamanho da palavra,
+percorre m*(n/2) vezes o laço
+Analisando se é o i-esimo termo, colocando o ultimo menos o i-esimo - 1
+E o contrário também é válido, assim percorrendo apenas metade das vezes a chave.
+*/
 char criptografa_abc(char s[])
 {   int i; //Contador i
     char chave[] ={'abcdefghijklmnopqrstuvxyz'} ;
-    int n = strlen(chave)
-    int m = strlen(s)
+    int n = strlen(chave);
+    int m = strlen(s);
     char *criptografado= malloc(m * sizeof(char));
     for(i=0;i<n/2;i++)
         {
@@ -19,8 +32,6 @@ char criptografa_abc(char s[])
                     {
                         criptografado[j] = chave[i];
                     }
-
-
                 }
         }
 }
